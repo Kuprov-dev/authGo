2efcdb5f0841af22558c3787ac22e32d65a0babb
Dmitry Belyaev <ddbelyaev@gmail.com>
2021-10-19 13:06:03 +0100

    updated current multiplexer to newservemux vs defaultservemux

--- cmd/main.go
+++ cmd/main.go
@@ -4,17 +4,50 @@ import (
 	"auth_service/pkg/auth"
 	"auth_service/pkg/conf"
 	"fmt"
-	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	config := conf.New()
 
-	http.HandleFunc("/login", auth.SignInHandler(config))
-	http.HandleFunc("/logout", auth.SignOutHandler(config))
-	http.HandleFunc("/test", auth.Test)
+	PORT := ":8080"
 
-	fmt.Println("Starting server")
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	mux := http.NewServeMux()
+	s := &http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		IdleTimeout:  10 * time.Second,
+		ReadTimeout:  time.Second,
+		WriteTimeout: time.Second,
+	}
+	defer s.Close()
+
+	fmt.Println("Server started...")
+
+	mux.Handle("/login", auth.SignInHandler(config))
+	mux.Handle("/logout", auth.SignOutHandler(config))
+	mux.Handle("/test", http.HandlerFunc(auth.Test))
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Println(err)
+			return
+		}
+	}()
+
+	<-stop
+
+	fmt.Println("Server stopped...")
 }
