ba9c0cac2a241a548fb214b39dfc58af576796e1
Eldar <eldar100@mail.ru>
2021-10-23 17:14:22 +0000

    pass user creds through context in middleware

--- pkg/jwt/middlewares.go
+++ pkg/jwt/middlewares.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"strings"
 
-	"auth_service/"
+	"auth_service/models"
 
 	"github.com/golang-jwt/jwt"
 )
@@ -15,32 +15,48 @@ type ErrorMsg struct {
 	Message string `json:"message"`
 }
 
+type UserKey string
+var userKey UserKey = "user"
+
 // TODO: Access user through user interface + pass user data in ctx <23-10-21, ddbelyaev> //
 // This is a JWT validating middleware. It's purpose is to validate JWT before allowing users
 // request to fall through to next middleware. As a result, it passes on user info in context (ctx).
 func ValidateTokenMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		authorizationHeader := req.Cookie("Access") //Header.Get("authorization")
+		refreshHeader := req.Cookie("Refresh") //Header.Get("authorization")
+
 		if authorizationHeader != "" {
 			bearerToken := strings.Split(authorizationHeader, " ")
 			if len(bearerToken) == 2 {
-				token, error := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
+				// token, error := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
+				// 	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				// 		return nil, fmt.Errorf("There was an error")
+				// 	}
+				// 	return []byte("secret"), nil
+				// })
+
+				claims := &Claims{}
+				token, err := jwt.ParseWithClaims(bearerToken[1], claims, func(token *jwt.Token) (interface{}, error) {
 					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 						return nil, fmt.Errorf("There was an error")
 					}
-					return []byte("secret"), nil
+					return []byte(secretKey), nil
 				})
+
 				if error != nil {
 					json.NewEncoder(w).Encode(ErrorMsg{Message: error.Error()})
 					return
 				}
+
 				if token.Valid {
+					var userCreds models.UserCredentials
 
-					var user User
-					type UserKey string
-					var userKey UserKey = "user"
+					userCreds.Username = claims.Username
+					userCreds.AccessToken = authorizationHeader
+					userCreds.RefreshToken = refreshHeader
 
-					ctx := context.WithValue(r.Context(), userKey, user)
+					ctx := context.WithValue(r.Context(), userKey, userCreds)
 					next(w, req)
 				} else {
 					json.NewEncoder(w).Encode(ErrorMsg{Message: "Invalid authorization token"})
